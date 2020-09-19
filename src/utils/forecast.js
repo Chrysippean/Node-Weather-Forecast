ff4b52884b632b43c68a9b9e030f1e0a0e71ae68
Chrysippean <danielnnguyen16@yahoo.com>
2020-09-19 15:09:19 -0700

    folders

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (latitude, longitude, callback) => {
 		} else if (body.error) {
 			callback('Unable to find location', undefined);
 		} else {
-			callback(undefined, "It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees.")
+			let report = "It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees. "
+			report += "There is a " + body.current.precip + "\% chance of rain. The humidity is " + body.current.humidity + ", and the cloud cover is " + body.current.cloudcover + "\%."
+			callback(undefined, report)
 		}
 	})
 
